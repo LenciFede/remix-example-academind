324adda60b306df4144b6fd1e9ad39ab071c2db0
Federico Lencina <federico.lencina@grupoesfera.com.ar>
2023-10-12 16:47:15 -0300

    feat: 🎸 add action and loader function to add and fetch data

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,14 +1,40 @@
-import type { LinksFunction } from "@remix-run/node";
+import {
+  redirect,
+  type ActionFunctionArgs,
+  type LinksFunction,
+} from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import NewNote, { links as newNotesLinks } from "~/components/NewNote";
+import NoteList, { links as newNotesLists } from "~/components/NoteList";
+
+import { getStoredNotes, storeNotes } from "~/data/notes";
 
 export default function NotesPage() {
+  const notes = useLoaderData();
+  console.log("notes: ", notes);
   return (
     <main>
       <NewNote />
+      <NoteList notes={notes} />
     </main>
   );
 }
 
+export const loader = async () => {
+  const notes = await getStoredNotes();
+  return notes;
+};
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const formData: FormData = await request.formData();
+  const noteData = Object.fromEntries(formData);
+  const existingNotes = await getStoredNotes();
+  noteData.id = new Date().toISOString();
+  const updateNotes = existingNotes.concat(noteData);
+  await storeNotes(updateNotes);
+  return redirect("/notes");
+};
+
 export const links: LinksFunction = () => {
-  return [...newNotesLinks()];
+  return [...newNotesLinks(), ...newNotesLists()];
 };
